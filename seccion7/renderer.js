5468c16f15db50a8180d8aab44e09e80ad7f6194
juanda <juanda@juandarodriguez.es>
2018-04-16 12:40:22 +0100

    creación de ventana modal en proceso renderer

--- seccion7/renderer.js
+++ seccion7/renderer.js
@@ -1,6 +1,17 @@
 const { webFrame } = require("electron");
+const { BrowserWindow } = require("electron").remote
 
 let zoom = 0
+
+document.getElementById("crear_ventana").addEventListener("click", () => {
+
+    let parentWindow = BrowserWindow.fromId(1)
+
+    let newWindow = new BrowserWindow({width: 600, heigth: 400, parent: parentWindow, modal: true})
+
+    newWindow.loadURL('http://google.es')
+
+})
 
 document.getElementById("zoom_in").addEventListener("click", () => {
     webFrame.setZoomLevel(++zoom)
